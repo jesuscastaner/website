37a09a6e28be85cee66b815314370599d0e5899e
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-09-20 23:26:33 +0200

    feat: set fira code as default font

--- astro.config.js
+++ astro.config.js
@@ -1,11 +1,20 @@
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   site: "https://jesuscastaner.com/",
   integrations: [mdx()],
   vite: {
     plugins: [tailwindcss()],
+  },
+  experimental: {
+    fonts: [
+      {
+        provider: fontProviders.google(),
+        name: "Fira Code",
+        cssVariable: "--font-fira-code",
+      },
+    ],
   },
 });
