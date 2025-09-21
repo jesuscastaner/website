ba7d0069c85a426c994db1fc646fa046d6d3ef9b
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-09-21 11:39:26 +0200

    feat: add posts page with wip layout

--- astro.config.js
+++ astro.config.js
@@ -1,10 +1,16 @@
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 import { defineConfig, fontProviders } from "astro/config";
 
 export default defineConfig({
   site: "https://jesuscastaner.com/",
-  integrations: [mdx()],
+  integrations: [
+    icon({
+      iconDir: "src/assets/icons",
+    }),
+    mdx(),
+  ],
   vite: {
     plugins: [tailwindcss()],
   },
