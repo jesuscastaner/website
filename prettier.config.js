dc93c90f2328e99d629e567b0e78917b3f4acce3
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-09-21 12:43:42 +0200

    chore: add prettier-plugin-tailwindcss

--- prettier.config.js
+++ prettier.config.js
@@ -6,7 +6,7 @@ export default {
   singleAttributePerLine: true,
 
   // plugin settings
-  plugins: ["prettier-plugin-astro"],
+  plugins: ["prettier-plugin-astro", "prettier-plugin-tailwindcss"],
   overrides: [
     {
       files: "*.astro",
