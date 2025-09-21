361077ce534021fdea876aa9d3013963824003f1
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-09-21 14:38:20 +0200

    refactor: centralize project consts and ts types

---
+++ src/consts.ts
@@ -0,0 +1,37 @@
+import type { SitePage } from "#types";
+
+/* site metadata */
+export const SITE = {
+  /**
+   * site name
+   */
+  name: "jesuscastaner.com",
+  /**
+   * site description
+   */
+  description: "Sitio web de Jesús Castañer.",
+  /**
+   * site language
+   */
+  lang: "es",
+} as const;
+
+/** site pages */
+export const PAGES = {
+  /**
+   * home page
+   */
+  home: {
+    name: "Inicio",
+    href: "/",
+    description: SITE.description,
+  },
+  /**
+   * posts page
+   */
+  posts: {
+    name: "Publicaciones",
+    href: "/posts",
+    description: "Publicaciones de Jesús Castañer.",
+  },
+} as const satisfies Record<string, SitePage>;
---
+++ src/types.ts
@@ -0,0 +1,15 @@
+/** site page */
+export type SitePage = {
+  /**
+   * name of the site page
+   */
+  name: string;
+  /**
+   * link to the site page
+   */
+  href: string;
+  /**
+   * description of the site page
+   */
+  description: string;
+};
