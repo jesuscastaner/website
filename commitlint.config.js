d5f016e458802cc1e1f2aeef311681f7ab9da4ed
jesuscastaner <113348003+jesuscastaner@users.noreply.github.com>
2025-09-26 21:35:03 +0200

    chore: support content commit type

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,4 +1,24 @@
 export default {
   // use conventional commits: https://www.conventionalcommits.org/en/v1.0.0/
   extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "build",
+        "chore",
+        "ci",
+        "content", // for content changes (e.g., posts)
+        "docs",
+        "feat",
+        "fix",
+        "perf",
+        "refactor",
+        "revert",
+        "style",
+        "test",
+      ],
+    ],
+  },
 };
